5b5a5d017982dcac7c86860b5d7acf0673328cd5
zjw <1219850250@qq.com>
2022-05-13 20:49:21 +0800

    lc208 实现前缀树

---
+++ src/leetcode/_208/ImplementTrieTree.java
@@ -0,0 +1,62 @@
+package leetcode._208;
+
+/**
+ * https://leetcode.cn/problems/implement-trie-prefix-tree/
+ */
+public class ImplementTrieTree {
+    public static void main(String[] args) {
+        Trie trie = new Trie();
+        trie.insert("apple");
+        System.out.println(trie.search("apple"));
+        System.out.println(trie.search("app"));
+        System.out.println(trie.startsWith("app"));
+        trie.insert("app");
+        System.out.println(trie.search("app"));
+    }
+}
+
+class Trie {
+    private final Trie[] children;
+    private boolean isEnd;
+
+    public Trie() {
+        children = new Trie[26];
+        isEnd = false;
+    }
+
+    public void insert(String word) {
+        Trie node = this;
+        char[] chars = word.toCharArray();
+        for (char ch : chars) {
+            int idx = ch - 'a';
+            if (node.children[idx] == null) {
+                node.children[idx] = new Trie();
+            }
+            node = node.children[idx];
+        }
+        node.isEnd = true;
+    }
+
+    public boolean search(String word) {
+        Trie node = searchPrefix(word);
+        return node != null && node.isEnd;
+    }
+
+    private Trie searchPrefix(String word) {
+        Trie node = this;
+        char[] chars = word.toCharArray();
+        for (char ch : chars) {
+            int idx = ch - 'a';
+            if (node.children[idx] == null) {
+                return null;
+            }
+            node = node.children[idx];
+        }
+        return node;
+    }
+
+    public boolean startsWith(String prefix) {
+        Trie node = searchPrefix(prefix);
+        return node != null;
+    }
+}
