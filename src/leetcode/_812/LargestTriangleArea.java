77aaeddb75a2d267a8c77d9e32416a2625acd13a
zjw <1219850250@qq.com>
2022-05-15 15:37:38 +0800

    lc812 最大三角形面积 枚举+三角形面积公式

--- src/leetcode/_812/LargestTriangleArea.java
+++ src/leetcode/_812/LargestTriangleArea.java
@@ -5,33 +5,27 @@ package leetcode._812;
  */
 public class LargestTriangleArea {
     public double largestTriangleArea(int[][] points) {
-        int INF = 100;
-        int minX = INF, maxX = -INF;
-        int minY = INF, maxY = -INF;
-
-        for (int[] point : points) {
-            if (point[0] == 0) {
-                minX = Math.min(minX, point[1]);
-                maxX = Math.max(maxX, point[1]);
-            }
-            if (point[1] == 0) {
-                minY = Math.min(minY, point[0]);
-                maxY = Math.max(maxY, point[0]);
+        double maxArea = 0.0;
+        for (int i = 0; i < points.length - 2; i++) {
+            for (int j = i + 1; j < points.length - 1; j++) {
+                for (int k = j + 1; k < points.length; k++) {
+                    double area = triangleArea(points[i][0], points[i][1], points[j][0], points[j][1], points[k][0], points[k][1]);
+                    maxArea = Math.max(maxArea, area);
+                }
             }
         }
+        return maxArea;
+    }
 
-        if (minX == INF || minY == INF) {
-            return 0.0;
-        }
-        int x = maxX - minX;
-        int h = Math.max(Math.abs(minY), Math.abs(maxY));
-        return 0.5 * x * h;
+    // 三角形面积公式
+    private double triangleArea(int x1, int y1, int x2, int y2, int x3, int y3) {
+        return 0.5 * Math.abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2));
     }
 
     public static void main(String[] args) {
         LargestTriangleArea lta = new LargestTriangleArea();
-//        int[][] points = new int[][] {{0,0},{0,1},{1,0},{0,2},{2,0}};
-        int[][] points = new int[][] {{1,0},{0,0},{0,1}};
+        int[][] points = new int[][] {{0,0},{0,1},{1,0},{0,2},{2,0}};
+//        int[][] points = new int[][] {{1,0},{0,0},{0,1}};
         System.out.println(lta.largestTriangleArea(points));
     }
 }
