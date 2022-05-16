4017895bc10e3e0bf77ccc26bf38b46c6e4354e5
zjw <1219850250@qq.com>
2022-05-16 20:18:12 +0800

    lc173 二叉搜索树迭代器 递归树找后继

--- src/leetcode/_173/BinarySearchTreeIterator.java
+++ src/leetcode/_173/BinarySearchTreeIterator.java
@@ -38,3 +38,48 @@ class BSTIterator {
         return idx < inorderList.size();
     }
 }
+
+class BSTIterator1 {
+    TreeNode rootNode;
+    TreeNode cur;
+
+    public BSTIterator1(TreeNode root) {
+        this.rootNode = root;
+        cur = findFirst(root);
+    }
+
+    private TreeNode findFirst(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode left = findFirst(root.left);
+        return left != null ? left : root;
+    }
+
+    public int next() {
+        TreeNode tmpRoot = rootNode;
+        TreeNode tmpCur = cur;
+        int res = cur != null ? cur.val : 0;
+        cur = findNext(tmpRoot, tmpCur);
+        return res;
+    }
+
+    private TreeNode findNext(TreeNode root, TreeNode tmpCur) {
+        if (root == null) {
+            return null;
+        }
+        if (tmpCur.val >= root.val) {
+            return findNext(root.right, tmpCur);
+        }
+        TreeNode left = findNext(root.left, tmpCur);
+        if (left == null) {
+            return root;
+        }
+        return left.val < root.val ? left : root;
+    }
+
+    public boolean hasNext() {
+        return cur != null;
+    }
+}
