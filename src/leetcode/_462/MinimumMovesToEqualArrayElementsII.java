c298840d59dad90e1d1cd8714669515efc4d62fd
zjw <1219850250@qq.com>
2022-05-19 01:33:39 +0800

    lc462 最小移动次数使数组元素相等II 快速选择取中位数

--- src/leetcode/_462/MinimumMovesToEqualArrayElementsII.java
+++ src/leetcode/_462/MinimumMovesToEqualArrayElementsII.java
@@ -1,6 +1,7 @@
 package leetcode._462;
 
 import java.util.Arrays;
+import java.util.Random;
 
 /**
  * https://leetcode.cn/problems/minimum-moves-to-equal-array-elements-ii/
@@ -18,9 +19,52 @@ public class MinimumMovesToEqualArrayElementsII {
         return moves;
     }
 
+    Random random = new Random();
+    public int minMoves2QuickSelection(int[] nums) {
+        int n = nums.length;
+        int target = quickSelection(nums, 0, n - 1, n / 2);
+        int moves = 0;
+        for (int num : nums) {
+            moves += Math.abs(num - target);
+        }
+        return moves;
+    }
+
+    private int quickSelection(int[] nums, int left, int right, int idx) {
+        int i = randomSelection(nums, left, right);
+        if (i == idx) {
+            return nums[idx];
+        }
+        return i < idx ? quickSelection(nums, i + 1, right, idx) : quickSelection(nums, left, i - 1, idx);
+    }
+
+    private int randomSelection(int[] nums, int l, int r) {
+        int i = random.nextInt(r - l + 1) + l;
+        swap(nums, i, r);
+        return partition(nums, l, r);
+    }
+
+    private int partition(int[] nums, int l, int r) {
+        int x = nums[r], i = l - 1;
+        for (int j = l; j < r; j++) {
+            if (nums[j] <= x) {
+                i++;
+                swap(nums, i, j);
+            }
+        }
+        swap(nums, i + 1, r);
+        return i + 1;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+
     public static void main(String[] args) {
         MinimumMovesToEqualArrayElementsII mm = new MinimumMovesToEqualArrayElementsII();
         int[] nums = new int[] {1,0,0,8,6};
-        System.out.println(mm.minMoves2(nums));
+        System.out.println(mm.minMoves2QuickSelection(nums));
     }
 }
