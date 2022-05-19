44bbb8046c894eb845327a848af5941cd7bd0872
zjw <1219850250@qq.com>
2022-05-19 18:55:50 +0800

    lc75 颜色分类 双指针相向

--- src/leetcode/_75/SortColors.java
+++ src/leetcode/_75/SortColors.java
@@ -58,6 +58,27 @@ public class SortColors {
         }
     }
 
+    public void sortColors2PTowards(int[] nums) {
+        int n = nums.length;
+        int p0 = 0, p2 = n - 1;
+        for (int i = 0; i <= p2; i++) {
+            while (i <= p2 && nums[i] == 2) {
+                swap(nums, i, p2);
+                p2--;
+            }
+            if (nums[i] == 0) {
+                swap(nums, i, p0);
+                p0++;
+            }
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+
     public static void main(String[] args) {
         SortColors sc = new SortColors();
         int[] nums = new int[] {2,0,2,1,1,0};
