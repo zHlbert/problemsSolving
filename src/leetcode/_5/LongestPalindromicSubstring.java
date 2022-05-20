ce80a7d9187bc63983297b477742676df7031d07
zjw <1219850250@qq.com>
2022-05-20 18:08:32 +0800

    lc436 中心扩展法

--- src/leetcode/_5/LongestPalindromicSubstring.java
+++ src/leetcode/_5/LongestPalindromicSubstring.java
@@ -48,13 +48,37 @@ public class LongestPalindromicSubstring {
     }
 
     // TODO: 中心扩展
+    public String longestPalindromeExpand(String s) {
+        int n = s.length();
+        char[] chars = s.toCharArray();
+        int start = 0, end = 0;
+        for (int i = 0; i < n - 1; i++) {
+            int len1 = expandFromCenter(chars, i, i);
+            int len2 = expandFromCenter(chars, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start + 1) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private int expandFromCenter(char[] chars, int l, int r) {
+        while (l >= 0 && r < chars.length && chars[l] == chars[r]) {
+            l--;
+            r++;
+        }
+        return r - l - 1;
+    }
 
     public static void main(String[] args) {
         LongestPalindromicSubstring ls = new LongestPalindromicSubstring();
-        System.out.println(ls.longestPalindrome("babad"));
-        System.out.println(ls.longestPalindrome("babab"));
-        System.out.println(ls.longestPalindrome("a"));
-        System.out.println(ls.longestPalindrome("cbbd"));
-        System.out.println(ls.longestPalindrome("ac"));
+        System.out.println(ls.longestPalindromeExpand("babad"));
+        System.out.println(ls.longestPalindromeExpand("babab"));
+        System.out.println(ls.longestPalindromeExpand("a"));
+        System.out.println(ls.longestPalindromeExpand("cbbd"));
+        System.out.println(ls.longestPalindromeExpand("ac"));
+        System.out.println(ls.longestPalindromeExpand("bb"));
     }
 }
