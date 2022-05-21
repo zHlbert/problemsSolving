01ab1d3df83641ec744d5427a93fd4f4e4e13c9f
zjw <1219850250@qq.com>
2022-05-22 01:51:17 +0800

    lc464 我能赢吗 记忆化搜索+状态压缩

---
+++ src/leetcode/_464/CanIWin.java
@@ -0,0 +1,50 @@
+package leetcode._464;
+
+public class CanIWin {
+    int maxChoosable, total;
+    Boolean[] memo;
+    public boolean canIWin(int maxChoosableInteger, int desiredTotal) {
+        if (maxChoosableInteger >= desiredTotal) {
+            return true;
+        }
+        if ((maxChoosableInteger + 1) * maxChoosableInteger < desiredTotal) {
+            return false;
+        }
+        maxChoosable = maxChoosableInteger;
+        total = desiredTotal;
+        memo = new Boolean[1 << maxChoosableInteger];
+        return dfs(0, 0);
+    }
+
+    /**
+     * 记忆化搜索+状态压缩
+     * @param usedIntegers
+     * @param curTotal
+     * @return
+     */
+    private boolean dfs(int usedIntegers, int curTotal) {
+        if (memo[usedIntegers] != null) {
+            return memo[usedIntegers];
+        }
+        for (int i = 0; i < maxChoosable; i++) {
+            if (((usedIntegers >> i) & 1) == 1) {
+                continue;
+            }
+            // 当前加和 >= 需要和 或 下一个人输掉，则赢
+            if (curTotal + i + 1 >= total
+                    || !dfs(usedIntegers | (1 << i), curTotal + i + 1)) {
+                return memo[usedIntegers] = true;
+            }
+        }
+        return memo[usedIntegers] = false;
+    }
+
+    public static void main(String[] args) {
+        CanIWin cw = new CanIWin();
+        int maxChoosableInteger = 6, desiredTotal = 1;
+        System.out.println(cw.canIWin(maxChoosableInteger, desiredTotal));
+//        for (int i = 0; i < cw.memo.length; i++) {
+//            System.out.println(i + ": " + cw.memo[i]);
+//        }
+    }
+}
