8f579bedf8f7c04011409a8a5966f5f50c8d48c6
zjw <1219850250@qq.com>
2022-05-27 14:11:01 +0800

    lc16 最接近的三数之和 双指针

--- src/leetcode/_16/ThreeSumClosest.java
+++ src/leetcode/_16/ThreeSumClosest.java
@@ -22,6 +22,9 @@ public class ThreeSumClosest {
         Arrays.sort(nums);
         int n = nums.length;
         for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
             int left = i + 1, right = n - 1;
             while (left < right) {
                 int curSum = nums[i] + nums[left] + nums[right];
