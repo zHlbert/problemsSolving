07d024d636bc58dee80a32fbc06721b4b51a3937
zjw <1219850250@qq.com>
2022-05-27 22:46:11 +0800

    lc437 路径总和III 前缀和

--- src/leetcode/_437/PathSumIII.java
+++ src/leetcode/_437/PathSumIII.java
@@ -3,7 +3,9 @@ package leetcode._437;
 import utils.TreeNode;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * https://leetcode.cn/problems/path-sum-iii/
@@ -60,6 +62,33 @@ public class PathSumIII {
                 + (targetSum == root.val ? 1 : 0);
     }
 
+    // 前缀和对应的数量
+    Map<Integer, Integer> preSumCount;
+    int targetSum;
+    public int pathSum1(TreeNode root, int targetSum) {
+        this.targetSum = targetSum;
+        preSumCount = new HashMap<>();
+        // 表示前缀和为0的节点为空，有一个空。否则若curSum = targetSum，将错过从root到i这条路径
+        preSumCount.put(0, 1);
+        return preSumBacktrack(root, 0);
+    }
+
+    private int preSumBacktrack(TreeNode root, int curSum) {
+        if (root == null) {
+            return 0;
+        }
+        curSum += root.val;
+        // sum(i,j) = preSum(i) - preSum(j) = targetSum
+        // curSum为根节点到当前节点的和
+        int cnt = preSumCount.getOrDefault(curSum - targetSum, 0);
+
+        preSumCount.put(curSum, preSumCount.getOrDefault(curSum, 0) + 1);
+        cnt += preSumBacktrack(root.left, curSum);
+        cnt += preSumBacktrack(root.right, curSum);
+        preSumCount.put(curSum, preSumCount.get(curSum) - 1);
+        return cnt;
+    }
+
     public static void main(String[] args) {
         PathSumIII ps3 = new PathSumIII();
         TreeNode root = new TreeNode(10);
@@ -73,7 +102,7 @@ public class PathSumIII {
         root.left.right.right = new TreeNode(1);
 
         int targetSum = 8;
-        System.out.println(ps3.pathSum(root, targetSum));
+        System.out.println(ps3.pathSum1(root, targetSum));
 //        System.out.println(ps3.res);
     }
 }
