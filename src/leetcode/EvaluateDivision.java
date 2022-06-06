b303a7eb252c70332fc7d2c5dc07d39987292aa9
zjw <1219850250@qq.com>
2022-06-06 22:39:09 +0800

    lc399 除法求值 并查集

--- src/leetcode/EvaluateDivision.java
+++ src/leetcode/EvaluateDivision.java
@@ -77,6 +77,44 @@ public class EvaluateDivision {
         return -1.0;
     }
 
+    public double[] calcEquationUnion(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        int eqSize = equations.size();
+        // 并查集
+        UnionFind unionFind = new UnionFind(2 * eqSize);
+        // 变量与id关系
+        Map<String, Integer> varIdxMap = new HashMap<>();
+        int id = 0;
+        for (int i = 0; i < eqSize; i++) {
+            List<String> eq = equations.get(i);
+            String var1 = eq.get(0);
+            String var2 = eq.get(1);
+            if (!varIdxMap.containsKey(var1)) {
+                varIdxMap.put(var1, id++);
+            }
+            if (!varIdxMap.containsKey(var2)) {
+                varIdxMap.put(var2, id++);
+            }
+            unionFind.union(varIdxMap.get(var1), varIdxMap.get(var2), values[i]);
+        }
+
+        int qSize = queries.size();
+        double[] res = new double[qSize];
+        for (int i = 0; i < qSize; i++) {
+            List<String> q = queries.get(i);
+            String var1 = q.get(0);
+            String var2 = q.get(1);
+
+            Integer id1 = varIdxMap.get(var1);
+            Integer id2 = varIdxMap.get(var2);
+            if (id1 == null || id2 == null) {
+                res[i] = -1.0d;
+            } else {
+                res[i] = unionFind.isConnected(id1, id2);
+            }
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         EvaluateDivision ed = new EvaluateDivision();
         String[][] eqs = new String[][] {{"a","b"},{"b","c"}};
@@ -90,7 +128,7 @@ public class EvaluateDivision {
         for (String[] q : qs) {
             queries.add(new ArrayList<>(Arrays.asList(q)));
         }
-        double[] res = ed.calcEquation(equations, values, queries);
+        double[] res = ed.calcEquationUnion(equations, values, queries);
         for (double re : res) {
             System.out.println(re);
         }
@@ -117,5 +155,51 @@ class Pair {
     @Override
     public int hashCode() {
         return Objects.hash(left, right);
+    }
+}
+
+class UnionFind {
+    private int[] parent;
+
+    private double[] weight;
+
+    public UnionFind(int n) {
+        parent = new int[n];
+        weight = new double[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            weight[i] = 1.0d;
+        }
+    }
+
+    public void union(int x, int y, double value) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX == rootY) {
+            return;
+        }
+        parent[rootX] = rootY;
+        // weight[rootX] * weight[x] == weight[y] * value
+        weight[rootX] = weight[y] * value / weight[x];
+    }
+
+    private int find(int x) {
+        if (x != parent[x]) {
+            int o = parent[x];
+            // x的父节点指向根节点
+            parent[x] = find(parent[x]);
+            // 权重为到根节点链路上权重之积
+            weight[x] *= weight[o];
+        }
+        return parent[x];
+    }
+
+    public double isConnected(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX == rootY) {
+            return weight[x] / weight[y];
+        }
+        return -1.0d;
     }
 }
