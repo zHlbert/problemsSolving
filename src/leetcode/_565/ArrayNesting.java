7879ec144c6c6e423775172ff9f483017c1623ab
zjw <1219850250@qq.com>
2022-07-16 19:35:35 +0800

    lc565 数组嵌套 循环

--- src/leetcode/_565/ArrayNesting.java
+++ src/leetcode/_565/ArrayNesting.java
@@ -27,10 +27,58 @@ public class ArrayNesting {
         dfs(nums, nums[i], c + 1);
     }
 
+    /**
+     * 循环
+     * @param nums
+     * @return
+     */
+    public int arrayNesting1(int[] nums) {
+        int n = nums.length;
+        boolean[] visited = new boolean[n];
+        int maxL = 0;
+        for (int i = 0; i < n; i++) {
+            int c = 0;
+            if (!visited[i]) {
+                int index = nums[i];
+                while (!visited[index]) {
+                    visited[index] = true;
+                    c++;
+                    index = nums[index];
+                }
+            }
+            maxL = Math.max(maxL, c);
+        }
+        return maxL;
+    }
+
+    /**
+     * 循环 修改原数组
+     * @param nums
+     * @return
+     */
+    public int arrayNesting2(int[] nums) {
+        int n = nums.length;
+        int maxL = 0;
+        for (int i = 0; i < n; i++) {
+            int c = 0;
+            if (nums[i] != -1) {
+                int index = nums[i];
+                while (nums[index] != -1) {
+                    c++;
+                    int next = nums[index];
+                    nums[index] = -1;
+                    index = next;
+                }
+                maxL = Math.max(maxL, c);
+            }
+        }
+        return maxL;
+    }
+
     public static void main(String[] args) {
         ArrayNesting an = new ArrayNesting();
         int[] nums = new int[] {5,4,0,3,1,6,2};
 //        int[] nums = new int[] {0,1,2};
-        System.out.println(an.arrayNesting(nums));
+        System.out.println(an.arrayNesting1(nums));
     }
 }
