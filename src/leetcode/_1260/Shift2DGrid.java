398137be77019255b50cadcfa2e67abeef7d7dc5
zjw <1219850250@qq.com>
2022-07-19 22:06:44 +0800

    lc1260 二维网格迁移

---
+++ src/leetcode/_1260/Shift2DGrid.java
@@ -0,0 +1,33 @@
+package leetcode._1260;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Shift2DGrid {
+    public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+        int m = grid.length, n = grid[0].length;
+        int[][] res = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                res[(i + (j + k) / n) % m][(j + k) % n] = grid[i][j];
+            }
+        }
+
+        // List<List<Integer>> s = new ArrayList<>();
+        // for (int i = 0; i < m; i++) {
+        //     List<Integer> r = new ArrayList<>();
+        //     for (int j = 0; j < n; j++) {
+        //         r.add(res[i][j]);
+        //     }
+        //     s.add(r);
+        // }
+        // return s;
+        List<List<Integer>> s = new ArrayList<>();
+        for (int[] re : res) {
+            s.add(Arrays.stream(re).boxed().collect(Collectors.toList()));
+        }
+        return s;
+    }
+}
