1be758bee69b34d442e25c5d3e55be3cc5692df0
zjw <1219850250@qq.com>
2022-07-30 13:45:33 +0800

    lc offerII 095 最长公共子序列 动态规划

---
+++ src/leetcode/offerII_95/LongestCommonSubsequence.java
@@ -0,0 +1,28 @@
+package leetcode.offerII_95;
+
+/**
+ * https://leetcode.cn/problems/qJnOS7/
+ */
+public class LongestCommonSubsequence {
+    /**
+     * 动态规划
+     * @param text1
+     * @param text2
+     * @return
+     */
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length(), n = text2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        char[] c1 = text1.toCharArray(), c2 = text2.toCharArray();
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (c1[i - 1] == c2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
