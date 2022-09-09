6c309df43c96fe00ead41b9102c1e5b0ff020bb2
zjw <1219850250@qq.com>
2022-09-09 10:58:02 +0800

    lc1598 文件夹操作日志搜集器

--- src/leetcode/_1598/CrawlerLogFolder.java
+++ src/leetcode/_1598/CrawlerLogFolder.java
@@ -4,15 +4,18 @@ public class CrawlerLogFolder {
     public int minOperations(String[] logs) {
         int top = 0;
         for (String log : logs) {
-            if (log.equals("./")) {
-                continue;
-            }
             if (log.equals("../")) {
                 top = Math.max(0, top - 1);
-            } else {
+            } else if (!log.equals("./")) {
                 top++;
             }
         }
         return top;
+    }
+
+    public static void main(String[] args) {
+        CrawlerLogFolder clf = new CrawlerLogFolder();
+        String[] logs = {"d2/", "./", "f4/", "./", "../", "3/"};
+        System.out.println(clf.minOperations(logs));
     }
 }
