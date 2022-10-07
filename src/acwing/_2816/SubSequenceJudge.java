18ef2daddd2746ed2074bb91b34b6fa87b2f0395
zjw <1219850250@qq.com>
2022-10-08 00:50:43 +0800

    acwing2816 判断子序列

---
+++ src/acwing/_2816/SubSequenceJudge.java
@@ -0,0 +1,43 @@
+package acwing._2816;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SubSequenceJudge {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] nm = reader.readLine().split(" ");
+        int n = Integer.parseInt(nm[0]);
+        int m = Integer.parseInt(nm[1]);
+        String[] As = reader.readLine().split(" ");
+        String[] Bs = reader.readLine().split(" ");
+        int[] A = new int[n];
+        for (int i = 0; i < n; i++) {
+            A[i] = Integer.parseInt(As[i]);
+        }
+        int[] B = new int[m];
+        for (int i = 0; i < m; i++) {
+            B[i] = Integer.parseInt(Bs[i]);
+        }
+        boolean isSeq = isSubsequence(A, B);
+        System.out.println(isSeq ? "Yes" : "No");
+        reader.close();
+    }
+
+    private static boolean isSubsequence(int[] A, int[] B) {
+        for (int i = 0, j = 0; i < A.length; i++) {
+            while (j < B.length && B[j] != A[i]) {
+                j++;
+            }
+            if (j >= B.length) {
+                break;
+            }
+            if (i == A.length - 1) {
+                return true;
+            }
+            j++;
+        }
+        return false;
+    }
+}
