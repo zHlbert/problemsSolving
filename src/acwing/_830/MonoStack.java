eb066b12f0f6d06b73db3a504d9d07ea593ada81
zjw <1219850250@qq.com>
2022-10-24 10:51:16 +0800

    acwing 830 单调栈

---
+++ src/acwing/_830/MonoStack.java
@@ -0,0 +1,31 @@
+package acwing._830;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MonoStack {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
+        String[] ns = reader.readLine().split(" ");
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++ ) {
+            a[i] = Integer.parseInt(ns[i]);
+        }
+        int[] st = new int[n];
+        int top = 0;
+        for (int i = 0; i < n; i++ ) {
+            while (top > 0 && a[st[top]] >= a[i]) {
+                top--;
+            }
+            if (top <= 0) {
+                System.out.print("-1 ");
+            } else {
+                System.out.print(a[st[top]] + " ");
+            }
+            st[++top] = i;
+        }
+        reader.close();
+    }
+}
