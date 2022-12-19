2cc07c63fe88013321fc4f6c255c5615b79ffe86
zjw <1219850250@qq.com>
2022-12-19 22:11:04 +0800

    lc 1971 寻找途中是否存在路径

--- src/leetcode/_1971/FindIfPathExistsInGraph.java
+++ src/leetcode/_1971/FindIfPathExistsInGraph.java
@@ -32,13 +32,31 @@ public class FindIfPathExistsInGraph {
         return false;
     }
 
-    // TODO: 2022/12/18 并查集 
+    // 并查集
+    int[] p;
+    public boolean validPath1(int n, int[][] edges, int source, int destination) {
+        p = new int[n];
+        for (int i = 0; i < n; i++) {
+            p[i] = i;
+        }
+
+        for (int[] edge : edges) {
+            p[find(edge[0])] = find(edge[1]);
+        }
+
+        return find(source) == find(destination);
+    }
+
+    private int find(int x) {
+        if (p[x] != x) p[x] = find(p[x]);
+        return p[x];
+    }
 
     public static void main(String[] args) {
         FindIfPathExistsInGraph fp = new FindIfPathExistsInGraph();
         int n = 3;
         int[][] edges = new int[][] {{0,1},{1,2},{2,0}};
         int source = 0, destination = 2;
-        System.out.println(fp.validPath(n, edges, source, destination));
+        System.out.println(fp.validPath1(n, edges, source, destination));
     }
 }
