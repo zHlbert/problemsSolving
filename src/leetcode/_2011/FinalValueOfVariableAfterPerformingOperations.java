853e77d9ac6e9d8cacd048f77bcdb7bc358f369d
zjw <1219850250@qq.com>
2022-12-22 23:24:18 +0800

    lc 2011 执行操作后的变量值

--- src/leetcode/_2011/FinalValueOfVariableAfterPerformingOperations.java
+++ src/leetcode/_2011/FinalValueOfVariableAfterPerformingOperations.java
@@ -3,10 +3,14 @@ package leetcode._2011;
 public class FinalValueOfVariableAfterPerformingOperations {
     public int finalValueAfterOperations(String[] operations) {
         int res = 0;
-        for (String op : operations) {
+        for (String op : operations)
             if (op.charAt(1) == '+') res ++;
             else res --;
-        }
         return res;
+    }
+
+    public static void main(String[] args) {
+        FinalValueOfVariableAfterPerformingOperations fv = new FinalValueOfVariableAfterPerformingOperations();
+        System.out.println(fv.finalValueAfterOperations(new String[] {"++X"}));
     }
 }
