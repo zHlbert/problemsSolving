61b86188984dc5241a564fc6c336774064ca7211
zjw <1219850250@qq.com>
2023-01-30 09:38:44 +0800

    lc 1926 迷宫中离入口最近的出口 bfs

--- src/leetcode/_1926/NearestExitFromEntranceInMaze.java
+++ src/leetcode/_1926/NearestExitFromEntranceInMaze.java
@@ -1,6 +1,8 @@
 package leetcode._1926;
 
+import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.Queue;
 
 public class NearestExitFromEntranceInMaze {
     int[] dx = new int[] {1, 0, -1, 0};
@@ -27,6 +29,29 @@ public class NearestExitFromEntranceInMaze {
                 if (nx >= 0 && nx < m && ny >= 0 && ny < n && maze[nx][ny] == '.' && d[nx][ny] == -1) {
                     d[nx][ny] = d[pt[0]][pt[1]] + 1;
                     q[++tt] = new int[] {nx, ny};
+                    if (nx == 0 || nx == m - 1 || ny == 0 || ny == n - 1) {
+                        return d[nx][ny];
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+
+    public int nearestExit1(char[][] maze, int[] entrance) {
+        int m = maze.length, n = maze[0].length;
+        int[][] d = new int[m][n];
+        for (int[] dd : d) Arrays.fill(dd, - 1);
+        Queue<int[]> q = new ArrayDeque<>();
+        q.offer(entrance);
+        d[entrance[0]][entrance[1]] = 0;
+        while (!q.isEmpty()) {
+            int[] pt = q.poll();
+            for (int i = 0; i < 4; i++) {
+                int nx = pt[0] + dx[i], ny = pt[1] + dy[i];
+                if (nx >= 0 && nx < m && ny >= 0 && ny < n && maze[nx][ny] == '.' && d[nx][ny] == -1) {
+                    d[nx][ny] = d[pt[0]][pt[1]] + 1;
+                    q.offer(new int[] {nx, ny});
                     if (nx == 0 || nx == m - 1 || ny == 0 || ny == n - 1) {
                         return d[nx][ny];
                     }
