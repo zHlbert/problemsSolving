de4759dffa3c760425d13698fa9521d943254c6a
zjw <1219850250@qq.com>
2023-02-02 15:30:55 +0800

    lc 1145 二叉树着色游戏 dfs

---
+++ src/leetcode/_1145/BinaryTreeColoringGame.java
@@ -0,0 +1,32 @@
+package leetcode._1145;
+
+import utils.TreeNode;
+
+public class BinaryTreeColoringGame {
+    /**
+     * dfs
+     * @param root
+     * @param n
+     * @param x
+     * @return
+     */
+    public boolean btreeGameWinningMove(TreeNode root, int n, int x) {
+        TreeNode node = findX(root, x);
+        int left = count(node.left);
+        int right = count(node.right);
+        int remain = n - 1 - left - right;
+        return (left << 1) > n || (right << 1) > n || (remain << 1) > n;
+    }
+
+    private int count(TreeNode node) {
+        if (node == null) return 0;
+        return 1 + count(node.left) + count(node.right);
+    }
+
+    private TreeNode findX(TreeNode node, int x) {
+        if (node == null || node.val == x) return node;
+        TreeNode res = findX(node.left, x);
+        if (res != null) return res;
+        return findX(node.right, x);
+    }
+}
