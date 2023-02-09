2cec669c61a6e71ed7543c45d31a915d69da2be3
zjw <1219850250@qq.com>
2023-02-10 01:00:31 +0800

    lc 1797 设计一个验证系统 哈希

--- src/leetcode/_1797/DesignAuthenticationManager.java
+++ src/leetcode/_1797/DesignAuthenticationManager.java
@@ -48,3 +48,51 @@ class AuthenticationManager {
         return tokens.size();
     }
 }
+
+class AuthenticationManager1 {
+
+    Map<String, Integer> tokens;
+    int ttl;
+    int N = 2010;
+    String[] ids = new String[N];
+    int[] exs = new int[N];
+    int idx = 0;
+    int start = 0;
+
+    public AuthenticationManager1(int timeToLive) {
+        tokens = new HashMap<>();
+        ttl = timeToLive;
+    }
+
+    public void generate(String tokenId, int currentTime) {
+        tokens.put(tokenId, currentTime + ttl);
+        ids[idx] = tokenId;
+        exs[idx++] = currentTime + ttl;
+    }
+
+    public void renew(String tokenId, int currentTime) {
+        if (!tokens.containsKey(tokenId)) return;
+        int ex = tokens.get(tokenId);
+        if (ex > currentTime) {
+            tokens.put(tokenId, currentTime + ttl);
+            ids[idx] = tokenId;
+            exs[idx++] = currentTime + ttl;
+        }
+    }
+
+    public int countUnexpiredTokens(int currentTime) {
+        // System.out.println("count: " + start + ", " + idx + ", " + currentTime + ", " + tokens);
+        int i = start;
+        for (; i < idx; i++) {
+            String tokenId = ids[i];
+            int ex = exs[i];
+            if (ex > currentTime) break;
+            // System.out.println(i + ": " + ex + ": " + tokenId);
+            if (tokens.containsKey(tokenId) && tokens.get(tokenId) <= currentTime) {
+                tokens.remove(tokenId);
+            }
+        }
+        start = i;
+        return tokens.size();
+    }
+}
