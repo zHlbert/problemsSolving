1d780355dbf19d5050fa93f2535975dac1b00e53
zjw <1219850250@qq.com>
2023-03-01 12:32:17 +0800

    interview 0502 二进制数转字符串

---
+++ src/leetcode/interview0502/BinaryNumberToStringLCCI.java
@@ -0,0 +1,14 @@
+package leetcode.interview0502;
+
+public class BinaryNumberToStringLCCI {
+    public String printBin(double num) {
+        StringBuilder sb = new StringBuilder("0.");
+        while (sb.length() <= 32 && num != 0) {
+            num *= 2;
+            int digit = (int) num;
+            sb.append(digit);
+            num -= digit;
+        }
+        return sb.length() <= 32 ? sb.toString() : "ERROR";
+    }
+}
