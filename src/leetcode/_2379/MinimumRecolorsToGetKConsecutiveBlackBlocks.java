76a3ecab83833ea669cc7aaccf498c013cbc93aa
zjw <1219850250@qq.com>
2023-03-08 13:07:31 +0800

    lc 2379 得到 K 个黑块的最少涂色次数 滑动窗口

---
+++ src/leetcode/_2379/MinimumRecolorsToGetKConsecutiveBlackBlocks.java
@@ -0,0 +1,21 @@
+package leetcode._2379;
+
+public class MinimumRecolorsToGetKConsecutiveBlackBlocks {
+    /**
+     * 滑动窗口
+     */
+    public int minimumRecolors(String blocks, int k) {
+        char[] bc = blocks.toCharArray();
+        int n = bc.length;
+        int res = k, wCnt = 0;
+        for (int i = 0, j = 0; i < n; i++) {
+            wCnt += (bc[i] == 'W' ? 1 : 0);
+            if (i - j + 1 == k) {
+                res = Math.min(res, wCnt);
+                wCnt -= (bc[j++] == 'W' ? 1 : 0);
+            }
+
+        }
+        return res;
+    }
+}
